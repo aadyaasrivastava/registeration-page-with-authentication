cb3fb5cca2ad071c9c5b79138d3706a8a243b87f
Aadyaa Srivastava <72547243+aadyaasrivastava@users.noreply.github.com>
2022-02-06 23:35:29 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+   
+        apiKey: "AIzaSyBgQFZEXvTbFpCIHHMBySRG6FQxEhnnaG8",
+        authDomain: "authenticate-app-e0cda.firebaseapp.com",
+        databaseURL: "https://authenticate-app-e0cda-default-rtdb.firebaseio.com",
+        projectId: "authenticate-app-e0cda",
+        storageBucket: "authenticate-app-e0cda.appspot.com",
+        messagingSenderId: "501596001372",
+        appId: "1:501596001372:web:721d5d9da4334ea2ac0299"
+      };
+    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
